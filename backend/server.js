872d7b3272eaa03672fb0b4e8ebc1dd8ea7a0db9
angelchen73 <73983768+angelchen73@users.noreply.github.com>
2021-09-12 21:26:48 -0500

    Preparation deploy to heroku

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import { ppid } from 'process';
 
 dotenv.config();
 
@@ -27,10 +28,6 @@ app.use(express.json());
 //     next();
 // });
 
-app.get('/', (req, res) => {
-	res.send('API is running');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -42,6 +39,16 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if(process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+	app.get('*', (req, res)=> res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))); 
+} else {	
+	app.get('/', (req, res) => {
+		res.send('API is running');
+	});
+}
 
 app.use(notFound);
 
